a9abc9bab0b7adf9c9c0ab7197e15b9b6d321aee
FredAbod <fredrickbolutife@gmail.com>
2025-09-01 14:56:34 +0100

    added database connection

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
-const express = require('express');
+const express = require("express");
 const dotenv = require("dotenv");
-const morgan = require('morgan');
+const morgan = require("morgan");
+const connectDB = require("./config/db");
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-
 app.listen(port, () => {
+  connectDB();
   console.log(`Server is running on port ${port}`);
 });
