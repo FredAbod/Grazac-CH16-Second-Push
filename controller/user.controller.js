ed1bf9adf83d1e2f33b5977de62bc1e437ec40f5
FredAbod <fredrickbolutife@gmail.com>
2025-09-03 15:39:54 +0100

    add updateUsername endpoint with authentication

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -64,8 +64,32 @@ const login = async (req, res) => {
 };
 
 
+const updateUsername = async (req, res) => {
+  try {
+    const { id } = req.user;
+    const { newUsername } = req.body;
+
+    if (!newUsername) {
+      return res.status(400).json({ message: "New username is required" });
+    }
+
+    const user = await User.findById(id);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    user.name = newUsername;
+    await user.save();
+
+    res.status(200).json({ message: "Username updated successfully" });
+  } catch (error) {
+    console.error("Error updating username:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
 
 module.exports = {
   signup,
   login,
+  updateUsername,
 };
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const { signup, login } = require('../controller/user.controller');
+const { signup, login, updateUsername } = require('../controller/user.controller');
+const { isAuthenticated } = require('../utils/auth');
 const route = express.Router();
 
 
 route.post('/signup', signup);
 route.post('/login', login);
-
-
+route.put('/updateUsername', isAuthenticated, updateUsername);
 
 module.exports = route;
