3797925f63fb9178770befc008723085fc8b531e
FredAbod <fredrickbolutife@gmail.com>
2025-09-15 15:41:46 +0100

    add rate limiting and security middleware; update dependencies in package.json and app.js

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+const cors = require("cors");
+// const sanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/user.routes");
 const adminRoutes = require("./routes/admin.routes");
@@ -10,13 +14,26 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+// app.use(sanitize())
+app.use(helmet());
 app.use(morgan("dev"));
+
+// Rate limiting middleware
+const rateLimiterMiddleware = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+  message: "Too many requests from this IP, please try again later."
+});
+
+app.use(rateLimiterMiddleware);
 
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
 
 app.use("/api/users", userRoutes);
 app.use("/api/admin", adminRoutes);
